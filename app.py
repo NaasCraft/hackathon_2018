27ae2f28c2f3100d59bd0f26f1314e98edafa374
Dimitrios Vasilas <dimitrios.vasilas@scality.com>
2018-01-22 20:33:31 +0100

    /goal and /start routes with db

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, abort
 from database import db, Game, GoalEvent
+import json
 
 app = Flask(__name__)
 
@@ -14,7 +15,27 @@ def goal():
         abort(401)
         return
     json_data = request.get_json(force=True)
-    return RESPONSE_STR
+    team = json_data['sensorID']
+    timestamp = json_data['timestamp']
+    goalEvent = GoalEvent(team=team, timestamp=timestamp)
+    db.session.add(goalEvent)
+    db.session.commit()
+    return json.dumps({}), 200, {'ContentType':'application/json'}
+
+@app.route('/start', methods=['POST'])
+def start():
+    if request.method != 'POST':
+        abort(401)
+        return
+    game_name = request.headers.get('name')
+    team1 = request.headers.get('team1')
+    team2 = request.headers.get('team2')
+    max_goals = request.headers.get('max_goals')
+    print(game_name, team1, team2, max_goals)
+    game = Game(name=game_name, score_red=0, score_blue=0, team_red=team1, team_blue=team2)
+    db.session.add(game)
+    db.session.commit()
+    return json.dumps({}), 200, {'ContentType':'application/json'}
 
 
 def main():
