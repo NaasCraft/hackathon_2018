ce81e25099d3df073dd15cf2c0d397c1f9f69e39
Guillaume Demonet <guillaume.demonet@scality.com>
2018-01-22 19:00:20 +0100

    First models for the database

---
+++ database.py
@@ -0,0 +1,18 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+class GoalEvent(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    team = db.Column(db.Integer, nullable=False)
+    timestamp = db.Column(db.Integer, nullable=False)
+
+
+class Game(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(60), nullable=False)
+    score_red = db.Column(db.Integer, nullable=False)
+    score_blue = db.Column(db.Integer, nullable=False)
+    team_red = db.Column(db.String(60), nullable=False)
+    team_blue = db.Column(db.String(60), nullable=False)
