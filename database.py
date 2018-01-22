4246d307dd9c38d1fd2944668ab43803cfd1b2a0
Guillaume Demonet <guillaume.demonet@scality.com>
2018-01-22 19:21:11 +0100

    Add start/stop fields for the Game model

--- database.py
+++ database.py
@@ -16,3 +16,7 @@ class Game(db.Model):
     score_blue = db.Column(db.Integer, nullable=False)
     team_red = db.Column(db.String(60), nullable=False)
     team_blue = db.Column(db.String(60), nullable=False)
+
+    # NOTE: for dates, we could use the `db.Datetime` type instead...
+    start = db.Column(db.Integer, nullable=False)
+    end = db.Column(db.Integer)
