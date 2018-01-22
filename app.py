4e95cac208d3c71605001cc23f8c7c5c0ff2d624
Dimitrios Vasilas <dimitrios.vasilas@scality.com>
2018-01-22 22:14:02 +0100

    fix sqlite issue

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 from flask import Flask, request, abort
-from database import db, Game, GoalEvent
+from database import app, db, Game, GoalEvent
 import json
-
-app = Flask(__name__)
 
 HOST = '127.0.0.1'
 PORT = 5444
@@ -15,9 +13,7 @@ def goal():
         abort(401)
         return
     json_data = request.get_json(force=True)
-    team = json_data['sensorID']
-    timestamp = json_data['timestamp']
-    goalEvent = GoalEvent(team=team, timestamp=timestamp)
+    goalEvent = GoalEvent(team=json_data['sensorID'], timestamp=json_data['timestamp'])
     db.session.add(goalEvent)
     db.session.commit()
     return json.dumps({}), 200, {'ContentType':'application/json'}
@@ -27,12 +23,7 @@ def start():
     if request.method != 'POST':
         abort(401)
         return
-    game_name = request.headers.get('name')
-    team1 = request.headers.get('team1')
-    team2 = request.headers.get('team2')
-    max_goals = request.headers.get('max_goals')
-    print(game_name, team1, team2, max_goals)
-    game = Game(name=game_name, score_red=0, score_blue=0, team_red=team1, team_blue=team2)
+    game = Game(name=request.headers.get('name'), score_red=0, score_blue=0, team_red=request.headers.get('team1'), team_blue=request.headers.get('team2'))
     db.session.add(game)
     db.session.commit()
     return json.dumps({}), 200, {'ContentType':'application/json'}
@@ -40,8 +31,8 @@ def start():
 
 def main():
     db.init_app(app)
-    app.run(host=HOST, port=PORT, debug=True)
     db.create_all()
+    app.run(host=HOST, port=PORT, debug=True)
 
 
 if __name__ == '__main__':
