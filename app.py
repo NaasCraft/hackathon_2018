85c8a19b7169e5134351cc563e4adbe4739b0239
Guillaume Demonet <guillaume.demonet@scality.com>
2018-01-23 12:03:33 +0100

    Clean up and add Game attributes

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 
+from functools import wraps
 import json
+import time
+
 from flask import Flask, request, abort
+
 from database import db_session, init_db
 from models import Game, GoalEvent
 
@@ -9,25 +13,59 @@ app = Flask(__name__)
 HOST = '127.0.0.1'
 PORT = 5444
 
+
 class Table(object):
     def __init__(self):
         self.in_use = False
         self.game_id = None
+
     def reserve(self):
         if self.in_use:
             return False
-        else:
-            self.in_use = True
-            return True
+        self.in_use = True
+        return True
+
     def start_game(self, game_id):
         self.game_id = game_id
+
     def release(self):
         self.in_use = False
         self.game_id = None
+
     def get_game(self):
         return self.game_id
 
+
 table = Table()
+
+
+def reserve_table(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        if not table.reserve():
+            abort(401, {'error': 'Table is occupied.'})
+            return
+        return func(*args, **kwargs)
+    return wrapper
+
+
+def get_game(game_id: 'Union[str, int]'):
+    game = Game.query.get(int(game_id))
+    if game is None:
+        abort(404, {'error': 'No such game.'})
+        return
+    return game
+
+
+def compute_duration(game: Game):
+    if game.paused:
+        return game.last_duration
+    now = int(time.time())
+    duration = now - game.start
+    if game.last_duration is not None:
+        duration += game.last_duration
+    return duration
+
 
 @app.route('/goal', methods=['POST'])
 def goal():
@@ -40,116 +78,133 @@ def goal():
     db_session.add(goal_event)
 
     current_game = table.get_game()
-    if current_game == None:
-        abort(401, {'error':'no_active_game'})
+    if current_game is None:
+        abort(404, {'error': 'No active game.'})
         return
-    game = Game.query.get(current_game)
-    if game == None:
-        abort(401, {'error':'no_such_game'})
-        return
+
+    game = get_game(current_game)
+
     if goal_event.team == '1':
         game.score_red += 1
-    elif goal_event.team == '2'  :
+
+    elif goal_event.team == '2':
         game.score_blue += 1
 
     db_session.commit()
 
-    body = json.dumps(goal_event.serialize())
-    return body, 201, {'ContentType': 'application/json'}
+    return app.response_class(
+        response=json.dumps(goal_event.serialize()),
+        status=201,
+        mimetype='application/json'
+    )
+
+
+@app.route('/start', methods=['POST'])
+@reserve_table
+def start():
+    json_data = request.get_json(force=True)
+
+    game = Game(
+        name=json_data['name'],
+        score_red=0,
+        score_blue=0,
+        team_red=json_data['team1'],
+        team_blue=json_data['team2'],
+        max_goals=json_data.get('max_goals', None),
+        start=int(time.time())
+    )
+
+    db_session.add(game)
+    db_session.commit()
+
+    table.start_game(game.id)
+
+    return app.response_class(
+        response=json.dumps(game.serialize()),
+        status=201,
+        mimetype='application/json'
+    )
+
+
+@app.route('/pause/<game_id>', methods=['POST'])
+def pause(game_id):
+    game = get_game(game_id)
+    game.last_duration = compute_duration(game)
+    game.paused = True
+    db_session.commit()
+
+    table.release()
+    return '{}', 200, {'ContentType': 'application/json'}
+
+
+@app.route('/resume/<game_id>', methods=['POST'])
+@reserve_table
+def resume(game_id):
+    game = get_game(game_id)
+    game.paused = False
+    game.start = int(time.time())
+    db_session.commit()
+
+    table.start_game(int(game.id))
+    return '{}', 200, {'ContentType': 'application/json'}
+
+
+@app.route('/end/<game_id>', methods=['POST'])
+def end(game_id):
+    game = get_game(game_id)
+    game.last_duration = compute_duration(game)
+    game.end = int(time.time())
+
+    table.release()
+
+    return app.response_class(
+        response=json.dumps(game.serialize()),
+        status=200,
+        mimetype='application/json'
+    )
+
+
+@app.route('/status/<game_id>', methods=['GET'])
+def status(game_id):
+    game = get_game(game_id)
+    # TODO: add duration
+    body = game.serialize()
+    body.update({'duration': compute_duration(game)})
+
+    return app.response_class(
+        response=json.dumps(body),
+        status=200,
+        mimetype='application/json'
+    )
+
+
+@app.route('/update/<game_id>', methods=['POST'])
+def update(game_id):
+    json_data = request.get_json(force=True)
+    game = get_game(game_id)
+
+    team = int(json_data['team'])
+    if team not in [1, 2]:
+        abort(400, "Unknown provided team (must be 1 or 2).")
+        return
+
+    difference = int(json_data['difference'])
+    game.score_red += difference if team == 1 else 0
+    game.score_blue += difference if team == 2 else 0
+
+    db_session.commit()
+    return app.response_class(
+        response=json.dumps(game.serialize()),
+        status=200,
+        mimetype='application/json'
+    )
 
 
 @app.teardown_appcontext
 def shutdown_session(exception=None):
     db_session.remove()
 
-@app.route('/start', methods=['POST'])
-def start():
-    json_data = request.get_json(force=True)
 
-    if table.reserve():
-        game = Game(
-            name=json_data['name'],
-            score_red=0,
-            score_blue=0,
-            team_red=json_data['team1'],
-            team_blue=json_data['team2'],
-            max_goals=json_data.get('max_goals', 10)
-        )
-        db_session.add(game)
-        db_session.commit()
-        table.start_game(int(game.id))
-        
-        body = json.dumps(game.serialize())
-        return body, 201, {'ContentType': 'application/json'}
-    else:
-        abort(401, {'error':'table_occupied'})
-        return
-
-@app.route('/pause/<game_id>', methods=['POST'])
-def pause(game_id):
-    game = Game.query.get(int(game_id))
-    if game == None:
-        abort(401, {'error':'no_such_game'})
-        return
-    game.paused = True
-    db_session.commit()
-    table.release()
-    return json.dumps({}), 200, {'ContentType':'application/json'}
-
-@app.route('/resume/<game_id>', methods=['POST'])
-def resume(game_id):
-    game = Game.query.get(int(game_id))
-    if game == None:
-        abort(401, {'error':'no_such_game'})
-        return
-    if table.reserve():
-        game.paused = False
-        db_session.commit()
-        table.start_game(int(game.id))
-        return json.dumps({}), 200, {'ContentType':'application/json'}
-    else:
-        abort(401, {'error':'table_occupied'})
-        return    
-
-
-@app.route('/end/<game_id>', methods=['POST'])
-def end(game_id):
-    game = Game.query.get(int(game_id))
-    if game == None:
-        abort(401, {'error':'no_such_game'})
-        return
-    table.release()
-    return json.dumps({}), 200, {'ContentType':'application/json'}
-
-@app.route('/status/<game_id>', methods=['GET'])
-def status(game_id):
-    game = Game.query.get(int(game_id))
-    #TODO // add duration
-    response = app.response_class(
-        response=json.dumps({'name':game.name, 'team_1_score':game.score_red, 'team_2_score':game.score_blue}),
-        status=200,
-        mimetype='application/json'
-    )
-    return response
-
-@app.route('/update/<game_id>', methods=['POST'])
-def update(game_id):
-    json_data = request.get_json(force=True)
-    game = Game.query.get(int(game_id))
-    if game == None:
-        abort(401, {'error':'no_such_game'})
-        return
-    if json_data['team'] == '1':
-        game.score_red += int(json_data['difference'])
-        db_session.commit()
-    elif json_data['team'] == '2':
-        game.score_blue += int(json_data['difference'])
-        db_session.commit()
-    else:
-        abort(401)
-        return
-    return json.dumps({}), 200, {'ContentType':'application/json'}
-    
 def main():
     init_db()
     app.run(host=HOST, port=PORT, debug=True)
--- models.py
+++ models.py
@@ -1,4 +1,6 @@
-from sqlalchemy import Column, Integer, String
+from datetime import datetime
+
+from sqlalchemy import Column, Boolean, Integer, String
 
 from database import Base
 
@@ -24,15 +26,26 @@ class Game(Base):
     name = Column(String(60), nullable=False)
     score_red = Column(Integer, nullable=False)
     score_blue = Column(Integer, nullable=False)
+
+    # TODO: move teams into another table, as a relationship
     team_red = Column(String(60), nullable=False)
     team_blue = Column(String(60), nullable=False)
-    max_goals = Column(Integer, nullable=False)
+
+    max_goals = Column(Integer, nullable=False, default=10)
+    paused = Column(Boolean, default=False)
+    start = Column(Integer, nullable=False)
+    end = Column(Integer)
+    last_duration = Column(Integer)
 
     def serialize(self):
-        return {
+        body = {
             key: getattr(self, key)
             for key in (
                 'id', 'name', 'score_blue', 'score_red', 'team_blue',
-                'team_red', 'max_goals'
+                'team_red', 'max_goals', 'paused'
             )
         }
+        body.update({'start': str(datetime.fromtimestamp(self.start))})
+        if self.end is not None:
+            body.update({'end': str(datetime.fromtimestamp(self.end))})
+        return body
